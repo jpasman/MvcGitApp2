968bd4f2386a998521fd42d46c735bffc3861597
jpasman <japasman@gmail.com>
2013-09-14 10:07:10 -0500

    updated comment on Index action in home controller

--- MvcGitApp2/Controllers/HomeController.cs
+++ MvcGitApp2/Controllers/HomeController.cs
@@ -10,7 +10,7 @@ namespace MvcGitApp2.Controllers
     {
         public ActionResult Index()
         {
-            ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
+            ViewBag.Message = "AZURE ASP.NET MVC application.";
 
             return View();
         }
